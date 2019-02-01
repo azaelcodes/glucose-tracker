39c2fc948f1b4140c040ce4f907f2875d0fdfb26
Ben Garcia <benji@mate1inc.com>
2019-02-01 16:51:07 -0500

    Added more niceties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { GlucoseListComponent } from './glucose-list/glucose-list.component';
-import { MatToolbarModule, MatListModule, MatExpansionModule, MatFormFieldModule } from '@angular/material';
+import { MatToolbarModule, MatListModule, MatExpansionModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 import { GlucoseItemComponent } from './glucose-item/glucose-item.component';
 import { FormsModule} from '@angular/forms';
 
@@ -21,7 +21,8 @@ import { FormsModule} from '@angular/forms';
     MatToolbarModule,
     FormsModule,
     MatExpansionModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
