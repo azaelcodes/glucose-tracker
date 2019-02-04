f96ce239ed261c2fd30347f32206d52493dfce0e
Ben Garcia <benji@mate1inc.com>
2019-02-04 10:10:09 -0500

    Added primary button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { GlucoseListComponent } from './glucose-list/glucose-list.component';
-import { MatToolbarModule, MatListModule, MatExpansionModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatToolbarModule, MatListModule, MatExpansionModule, MatFormFieldModule, MatInputModule, MatButtonModule } from '@angular/material';
 import { GlucoseItemComponent } from './glucose-item/glucose-item.component';
 import { FormsModule} from '@angular/forms';
 
@@ -22,7 +22,8 @@ import { FormsModule} from '@angular/forms';
     FormsModule,
     MatExpansionModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
