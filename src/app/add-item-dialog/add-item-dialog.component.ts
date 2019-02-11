85cc89f0b4af723783042e47ea78ef77e9af978e
Ben Garcia <benji@mate1inc.com>
2019-02-11 15:06:02 -0500

    Minor changes to the form

--- src/app/add-item-dialog/add-item-dialog.component.ts
+++ src/app/add-item-dialog/add-item-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
 import { CATEGORIES } from '../mock-categories';
+import {Glucose} from '../glucose';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-add-item-dialog',
@@ -10,11 +12,24 @@ import { CATEGORIES } from '../mock-categories';
 export class AddItemDialogComponent {
 
   categories = CATEGORIES;
+  items: Array<Glucose> = [];
+  item: Glucose;
 
   constructor(public dialogRef: MatDialogRef<AddItemDialogComponent>) { }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  addItem() {
+
+    const obj = new Glucose();
+    obj.userId = 1;
+    obj.level = 7.1;
+    obj.categoryId = 1;
+    obj.added = '2019-03-01';
+    this.items.push(obj);
+
   }
 
 }
