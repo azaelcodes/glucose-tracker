02c57acb405653d7bde2c15f735c21dab88ce98e
azaelgarcia <azael.codes@gmail.com>
2019-02-17 10:59:35 -0500

    Working with Service

--- src/app/glucose-list/glucose-list.component.ts
+++ src/app/glucose-list/glucose-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GlucoseService } from '../glucose.service';
-import {Glucose} from "../glucose";
+import {Glucose} from '../glucose';
 
 @Component({
   selector: 'app-glucose-list',
